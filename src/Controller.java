156764c21a2160d0a28cb73a46f625f0ebfdd1bf
David Brungardt <brungardtdb@gmail.com>
2020-12-13 13:49:56 -0700

    Initial commit, finished setting up main form for application.

---
+++ src/Controller.java
@@ -0,0 +1,58 @@
+import javafx.scene.control.Button;
+
+public class Controller {
+
+/*  Button Example*/
+
+    public Button button;
+    public void handleButtonClick()
+    {
+        System.out.println("Button has been clicked.");
+    }
+
+    public void handleAddParts()
+    {
+
+    }
+
+    public void handleModifyParts()
+    {
+
+    }
+
+    public void handleDeleteParts()
+    {
+
+    }
+
+    public void handleAddProducts()
+    {
+
+    }
+
+    public void handleModifyProducts()
+    {
+
+    }
+
+    public void handleDeleteProducts()
+    {
+
+    }
+
+    public  void handleExitForm()
+    {
+
+    }
+
+    public void handleSearchParts()
+    {
+
+    }
+
+    public void handleSearchProducts()
+    {
+
+    }
+
+}
