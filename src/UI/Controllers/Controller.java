d53182fea9546b9ffa30cdfe71f34a0b7f422647
David Brungardt <brungardtdb@gmail.com>
2020-12-22 21:51:50 -0700

    Finished logic for product form, made some UI changes in the CSS file.

--- src/UI/Controllers/Controller.java
+++ src/UI/Controllers/Controller.java
@@ -1,11 +1,8 @@
 package UI.Controllers;
-
 import InventoryAPI.Inventory;
 import InventoryAPI.Part;
 import InventoryAPI.Product;
-import com.sun.jdi.PrimitiveValue;
-import com.sun.scenario.effect.impl.prism.PrRenderInfo;
-import javafx.collections.ListChangeListener;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,7 +14,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
+import javafx.scene.input.*;
 import java.io.IOException;
 
 public class Controller {
@@ -30,8 +27,14 @@ public class Controller {
     @FXML private  TableColumn partNameColumn;
     @FXML private TableColumn partInventoryColumn;
     @FXML private TableColumn partCostColumn;
+    @FXML private TableView productTable;
+    @FXML private TableColumn productIDColumn;
+    @FXML private TableColumn productNameColumn;
+    @FXML private TableColumn productInventoryColumn;
+    @FXML private TableColumn productPriceColumn;
     @FXML private BorderPane mainBorderPane;
     @FXML private TextField partSearchTextField;
+    @FXML private TextField productSearchTextField;
 
 
     public void handleAddParts()
@@ -74,6 +77,14 @@ public class Controller {
                 partController.setPartToModify(getPartToModify());
                 partController.setInventory(inventory);
                 partController.setHomeController(this);
+
+                Stage stage = new Stage();
+                stage.initModality(Modality.APPLICATION_MODAL);
+                stage.initStyle(StageStyle.DECORATED);
+                stage.setTitle("Modify Parts");
+                stage.setScene(new Scene(root));
+                stage.show();
+
             }
             else {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -83,12 +94,6 @@ public class Controller {
                 alert.show();
             }
 
-            Stage stage = new Stage();
-            stage.initModality(Modality.APPLICATION_MODAL);
-            stage.initStyle(StageStyle.DECORATED);
-            stage.setTitle("Modify Parts");
-            stage.setScene(new Scene(root));
-            stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -109,6 +114,8 @@ public class Controller {
             alert.setContentText("Please select a part to delete from the part table!");
             alert.show();
         }
+        // Unselect parts in table after part is deleted
+        partTable.getSelectionModel().clearSelection();
     }
 
     public void handleAddProducts()
@@ -145,27 +152,55 @@ public class Controller {
         try {
             root = (Parent) fxmlLoader.load();
 
-            // Get controller so we can define if we are adding or modifying parts
-            ProductController productController = fxmlLoader.getController();
-            productController.setModifyProducts(false);
-            productController.setInventory(inventory);
-            productController.setHomeController(this);
-            productController.updateParts();
+            if (getProductToModify() != null)
+            {
+                // Get controller so we can define if we are adding or modifying parts
+                ProductController productController = fxmlLoader.getController();
+                productController.setModifyProducts(true);
+                productController.setProductToModify(getProductToModify());
+                productController.setInventory(inventory);
+                productController.setHomeController(this);
+                productController.updateParts();
 
+                Stage stage = new Stage();
+                stage.initModality(Modality.APPLICATION_MODAL);
+                stage.initStyle(StageStyle.DECORATED);
+                stage.setTitle("Modify Products");
+                stage.setScene(new Scene(root));
+                stage.show();
+            }
+            else
+            {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Input Invalid");
+                alert.setHeaderText("No product was selected!");
+                alert.setContentText("Please select a product to modify from the product table!");
+                alert.show();
+            }
 
-            Stage stage = new Stage();
-            stage.initModality(Modality.APPLICATION_MODAL);
-            stage.initStyle(StageStyle.DECORATED);
-            stage.setTitle("Modify Products");
-            stage.setScene(new Scene(root));
-            stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void handleDeleteProducts() {
+    public void handleDeleteProducts()
+    {
+        Product productToDelete = getProductToModify();
 
+        if (productToDelete != null)
+        {
+            inventory.deleteProduct(productToDelete);
+            updateProducts();
+            productTable.getSelectionModel().clearSelection();
+        }
+        else
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Input Invalid");
+            alert.setHeaderText("No product was selected!");
+            alert.setContentText("Please select a product to delete from the part table!");
+            alert.show();
+        }
     }
 
     @FXML  void handleExitForm()
@@ -181,11 +216,16 @@ public class Controller {
         ObservableList<Part> searchedParts;
         if (!partSearchTextField.getText().isEmpty())
         {
+            // Clear selection from table
+            partTable.getSelectionModel().clearSelection();
+
             try {
+                // First try to search for part ID
                 searchPartID = Integer.parseInt(partSearchTextField.getText());
                 searchedPart = this.inventory.lookupPart(searchPartID);
                 partTable.getSelectionModel().select(searchedPart);
             } catch (Exception e) {
+                // If ID cannot be parsed, try to search by name
                 searchedParts = this.inventory.lookupPart(partSearchTextField.getText());
                 searchedParts.forEach((part -> {
                     partTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
@@ -197,7 +237,28 @@ public class Controller {
 
     public void handleSearchProducts()
     {
+        int searchProductID;
+        Product searchedProduct;
+        ObservableList<Product> searchedProducts;
+        if (!productSearchTextField.getText().isEmpty())
+        {
+            // Clear selection from table
+           productTable.getSelectionModel().clearSelection();
 
+            try {
+                // First try to search for part ID
+                searchProductID = Integer.parseInt(productSearchTextField.getText());
+                searchedProduct = this.inventory.lookupProduct(searchProductID);
+                productTable.getSelectionModel().select(searchedProduct);
+            } catch (Exception e) {
+                // If ID cannot be parsed, try to search by name
+                searchedProducts = this.inventory.lookupProduct(productSearchTextField.getText());
+                searchedProducts.forEach((product -> {
+                    productTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+                    productTable.getSelectionModel().select(product);
+                }));
+            }
+        }
     }
 
     public void updateParts()
@@ -210,6 +271,9 @@ public class Controller {
         partCostColumn.setCellValueFactory(new PropertyValueFactory<Part, String>("price"));
 
         partTable.setItems(inventory.getAllParts());
+        // Unselect parts in table after part is deleted
+        partTable.getSelectionModel().clearSelection();
+        partTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
     }
 
     public Part getPartToModify()
@@ -221,15 +285,37 @@ public class Controller {
         return  null;
     }
 
-    public void updateProducts()
-    {
-
-    }
-
     public Product getProductToModify()
     {
-        return null;
+        if (productTable.getSelectionModel().getSelectedItem() != null)
+        {
+            return ((Product) productTable.getSelectionModel().getSelectedItem());
+        }
+        return  null;
     }
 
+    public void updateProducts()
+    {
+        ObservableList<Product> inventoryProducts = this.inventory.getAllProducts();
 
+        productIDColumn.setCellValueFactory(new PropertyValueFactory<Product, String>("id"));
+        productNameColumn.setCellValueFactory(new PropertyValueFactory<Product, String>("name"));
+        productInventoryColumn.setCellValueFactory(new PropertyValueFactory<Product, String>("stock"));
+        productPriceColumn.setCellValueFactory(new PropertyValueFactory<Product, String>("price"));
+
+        productTable.setItems(inventoryProducts);
+        // Unselect parts in table after part is updated
+        productTable.getSelectionModel().clearSelection();
+        productTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+    }
+
+    /**
+     * removes selection from part tables when form is clicked
+     * */
+    @FXML public void mainFormClicked(MouseEvent event)
+    {
+        // Unselect parts in table when user clicks on border pane
+        partTable.getSelectionModel().clearSelection();
+        productTable.getSelectionModel().clearSelection();
+    }
 }
--- src/UI/Controllers/ProductController.java
+++ src/UI/Controllers/ProductController.java
@@ -1,12 +1,14 @@
 package UI.Controllers;
+import InventoryAPI.Inventory;
 import InventoryAPI.Part;
+import InventoryAPI.Product;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
-import InventoryAPI.Inventory;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import javax.print.CancelablePrintJob;
 
 /**
  *
@@ -15,6 +17,8 @@ import javax.print.CancelablePrintJob;
 public class ProductController {
 
     private Inventory inventory;
+    private Product product;
+    private ObservableList<Part> associatedParts = FXCollections.observableArrayList();
     private static boolean modifyProducts;
     @FXML private Label addProductLabel;
     @FXML private  Button cancelProduct;
@@ -29,6 +33,11 @@ public class ProductController {
     @FXML private  TableColumn partNameColumn;
     @FXML private TableColumn partInventoryColumn;
     @FXML private TableColumn partCostColumn;
+    @FXML private TableView productPartTable;
+    @FXML private TableColumn productPartIDColumn;
+    @FXML private TableColumn productPartNameColumn;
+    @FXML private TableColumn productPartInventoryColumn;
+    @FXML private TableColumn productPartCostColumn;
     @FXML private TextField partSearchTextField;
     private int productID;
     private String productName;
@@ -49,6 +58,20 @@ public class ProductController {
     }
 
     /**
+     * @param product the product we are going to modify (only used when modifying product)
+     */
+    public void setProductToModify(Product product)
+    {
+        if (this.modifyProduct)
+        {
+            this.product = product;
+            this.associatedParts = product.getAllAssociatedParts();
+            updateAssociatedParts();
+            this.idTxt.setText(String.valueOf(this.product.getId()));
+        }
+    }
+
+    /**
      * @param controller the main form's controller, used to pass back information
      */
     public void setHomeController(Controller controller)
@@ -60,7 +83,7 @@ public class ProductController {
     /**
      * @param modifyProduct a boolean value that indicates if we are modifying products, (false for adding new product)
      */
-    public void setModifyParts(boolean modifyProduct)
+    public void setModifyProducts(boolean modifyProduct)
     {
         this.modifyProduct = modifyProduct;
 
@@ -122,6 +145,10 @@ public class ProductController {
         return  isValid;
     }
 
+    /**
+     * Searches for part in table
+     * If ID cannot be parsed, searches for part by name
+     * */
     public void handleSearchParts()
     {
         int searchPartID;
@@ -130,10 +157,12 @@ public class ProductController {
         if (!partSearchTextField.getText().isEmpty())
         {
             try {
+                // First try to search for part ID
                 searchPartID = Integer.parseInt(partSearchTextField.getText());
                 searchedPart = this.inventory.lookupPart(searchPartID);
                 partTable.getSelectionModel().select(searchedPart);
             } catch (Exception e) {
+                // If ID cannot be parsed, try to search by name
                 searchedParts = this.inventory.lookupPart(partSearchTextField.getText());
                 searchedParts.forEach((part -> {
                     partTable.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
@@ -143,6 +172,9 @@ public class ProductController {
         }
     }
 
+    /**
+     * updates parts in table based on parts in inventory
+     * */
     public void updateParts()
     {
         ObservableList<Part> inventoryParts = this.inventory.getAllParts();
@@ -155,34 +187,120 @@ public class ProductController {
         partTable.setItems(inventory.getAllParts());
     }
 
-    public void handleAddProducts()
+    /**
+     * updates parts in table based on parts associated with product
+     * */
+    public void updateAssociatedParts()
     {
+        productPartIDColumn.setCellValueFactory(new PropertyValueFactory<Part, String>("id"));
+        productPartNameColumn.setCellValueFactory(new PropertyValueFactory<Part, String>("name"));
+        productPartInventoryColumn.setCellValueFactory(new PropertyValueFactory<Part, String>("stock"));
+        productPartCostColumn.setCellValueFactory(new PropertyValueFactory<Part, String>("price"));
 
+        productPartTable.setItems(this.associatedParts);
     }
 
+    /**
+     * adds selected part to product and updates table for associated parts
+     * */
+    public void handleAddParts()
+    {
+        Part associatedPart = getPartToModify(partTable);
+
+        if (associatedPart != null)
+        {
+            this.associatedParts.add(associatedPart);
+            updateAssociatedParts();
+            // Unselect parts in table after part is added
+            partTable.getSelectionModel().select(null);
+            productPartTable.getSelectionModel().select(null);
+        }
+    }
+
+    /**
+     * @param tableView takes in a tableview for input, returns selected part (if any)
+     * @return returns the selected part if one is selected, otherwise returns null
+     * */
+    public Part getPartToModify(TableView tableView)
+    {
+        if (tableView.getSelectionModel().getSelectedItem() != null)
+        {
+            return ((Part) tableView.getSelectionModel().getSelectedItem());
+        }
+        return  null;
+    }
+
+    /**
+     * removes part association with product and removes from product part table
+     * */
     public void handleRemoveAssociatedPart()
     {
+        Part partToRemove = getPartToModify(productPartTable);
 
+        if (partToRemove != null)
+        {
+            this.associatedParts.remove(partToRemove);
+            updateAssociatedParts();
+            // Unselect parts in table after part is deleted
+            partTable.getSelectionModel().select(null);
+            productPartTable.getSelectionModel().select(null);
+        }
     }
 
+    /**
+     * saves changes to the product, closes product form and updates product table in main form
+     * */
     public void handleSaveProduct()
     {
+        Product newProduct;
 
+        if (inputIsValid())
+        {
+            if (modifyProduct)
+            {
+                // if we are modifying a product, use index to replace old product with new updated product
+                productID = this.product.getId();
+                newProduct = new Product(productID, productName, productCost, productStock, productMin, productMax);
+                // add associated parts to product and update
+                this.associatedParts.forEach((part -> {
+                    newProduct.addAssociatedPart(part);
+                }));
+                this.inventory.updateProduct(productID, newProduct);
+            }
+            else
+            {
+                // if we are creating a new product
+                productID = this.inventory.getAllProducts().size();
+                newProduct = new Product(productID, productName, productCost, productStock, productMin, productMax);
+                // add associated parts to product and add product to inventory
+                this.associatedParts.forEach((part -> {
+                    newProduct.addAssociatedPart(part);
+                }));
+                this.inventory.addProduct(newProduct);
+            }
+        }
+
+        this.homeController.updateProducts();
+        handleCancel();
     }
 
+    /**
+     * handles cancellation of product creation by closing the form
+     * */
     @FXML void handleCancel()
     {
         Stage stage = (Stage) cancelProduct.getScene().getWindow();
         stage.close();
     }
 
-    public void setModifyProducts(boolean modifyProducts)
+    /**
+     * @param event takes in a mouse click event
+     * removes selection from part tables when form is clicked
+     * */
+    @FXML private void handleOnMouseClicked(MouseEvent event)
     {
-        this.modifyProducts = modifyProducts;
-        if (this.modifyProducts)
-            addProductLabel.setText("Modify Products");
-        else
-            addProductLabel.setText("Add Products");
+        // Unselect parts in table when user clicks on border pane
+        partTable.getSelectionModel().clearSelection();
+        productPartTable.getSelectionModel().clearSelection();
     }
-
 }
